b478abd0f21849c1fff0d643d8abd73da85c3790
Andrew Birt <abirt1986@gmail.com>
2021-10-15 15:14:09 -0400

    add 10-15-2021 entry

--- scripts/journals/JournalDataProvider.js
+++ scripts/journals/JournalDataProvider.js
@@ -15,6 +15,13 @@ const journalData = [
     concepts: ["HTML", "CSS", "JavaScript"],
     entry: "Made progress on Martins's Aquarium, finished the Fish component and it's displaying to the DOM. I also setup Tip and Location data as well as components so when I have that data I can finish those components as well. They're already working in the project but they dont have any content. Tommy was knocking ketchup... his code is ketchup...",
     mood: "Confident"
+  },
+  {
+    id: 3,
+    date: "Oct 15 2021",
+    concepts: ["JavaScript", "Jest", "Unit Testing"],
+    entry: "I did the sowing chapter for Modern Farm. It was relatively easy to do, however, today has been exhausting looking for my tiny little nuances. For example, I spent a little bit of time getting the plant seeds function to populate the plant array simply because I used lowercase letters in my switch statement rather than capitalizing the seed names and that took some time for me to catch. Also I was helping my teammates with their projects where I almost missed that they forgot to import and call their component function in the main js module. What a way to end the week.",
+    mood: "Unsure"
   }
 ];
 
