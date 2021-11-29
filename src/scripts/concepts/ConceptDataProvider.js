eb2b151a6bd6f83b56659c0c4300cfd4b1c6b373
Andrew Birt <abirt1986@gmail.com>
2021-11-29 14:42:30 -0500

    add saveConcept function

--- src/scripts/concepts/ConceptDataProvider.js
+++ src/scripts/concepts/ConceptDataProvider.js
@@ -14,3 +14,13 @@ export const searchConcept = concept => {
   return fetch(`http://localhost:8088/concepts?name=${concept}`)
   .then(res => res.json());
 }
+
+export const saveConcept = concept => {
+  return fetch('http://localhost:8088/concepts', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(concept)
+  }).then(res => res.json());
+}
